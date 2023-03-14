dd9dba34cf84048b863647b6e6f45773f6077948
AkiyamaMio <xuanyuantec@gmail.com>
2023-03-14 09:08:24 +0800

    追加跨域，前后端联通

---
+++ src/views/EditPage/EditPage.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+  import { useGetFetch, usePostFetch } from '../../utils/fetch';
+
+  const data = {
+    name: 'k',
+    id: '1111'
+  }
+
+  usePostFetch('/example/exa', data).then( res => {
+    console.log(res)
+  })
+
+
+</script>
+
+<style lang="scss" src="./_.scss" scoped />
+
+<template>
+  <nav class="toolWrapper">
+    <ul>
+      <li>
+        创建书
+      </li>
+      <li>
+        创建分卷
+      </li>
+      <li>
+        创建章节
+      </li>
+      <li>
+        导入书
+      </li>
+      <li>
+        导出书
+      </li>
+    </ul>
+  </nav>
+  <div class="contentWrapper">
+    <div class="leftWrapper">left </div>
+    <div class="midWrapper">this authorAPP </div>
+    <div class="rightWrapper">right</div>
+  </div>
+</template>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import { useRoute, useRouter} from "vue-router"
+
+  const route = useRoute();
+  const router = useRouter();
+
+  const handleGoToHome = () => {
+    console.log(route)
+    console.log(router)
+    router.push('/home')
+  }
+</script>
+
+<template>
+  this login page
+  <button @click="handleGoToHome()">前往主页</button>
+</template>
