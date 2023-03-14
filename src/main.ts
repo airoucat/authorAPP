b033533211f3edf92d04cd678e3023fd7001fe85
AkiyamaMio <xuanyuantec@gmail.com>
2023-03-14 08:59:23 +0800

    追加跨域，前后端联通

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 
 import App from './App.vue'
 import router from './router'
@@ -8,6 +10,7 @@ import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(Antd)
 app.use(createPinia())
 app.use(router)
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,22 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginPage from '../views/LoginPage.vue'
+import EditPage from '../views/EditPage/EditPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'login',
+      component: LoginPage,
+      alias: '/login'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/edit',
+      name: 'edit',
+      component: EditPage
+    },
   ]
 })
 
